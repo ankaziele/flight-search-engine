7236e59ad71f8368d5615feb215deb7743cdae5f
U727184 <anna.zielinska@lhsystems.com>
2019-11-15 13:42:32 +0100

    highlight directive added with mouse hover effect

--- flight-search-engine/src/app/app.module.ts
+++ flight-search-engine/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OriginComponent } from './origin/origin.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OriginComponent
+    OriginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ flight-search-engine/src/app/highlight.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('rgb(21, 36, 68)', 'white');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null, null);
+  }
+
+  private highlight(backgroundColor: string, color: string) {
+    this.el.nativeElement.style.backgroundColor = backgroundColor;
+    this.el.nativeElement.style.color = color;
+  }
+}
