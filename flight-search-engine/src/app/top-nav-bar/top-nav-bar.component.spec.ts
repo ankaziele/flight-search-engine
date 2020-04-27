8ad55322909ac3e6cd6a159e94d47eb0f98bb336
ankaziele <ankaziele@gmail.com>
2020-04-27 09:54:50 +0200

    unit test for url path change

--- flight-search-engine/src/app/top-nav-bar/top-nav-bar.component.spec.ts
+++ flight-search-engine/src/app/top-nav-bar/top-nav-bar.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TopNavBarComponent } from './top-nav-bar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { Location } from '@angular/common';
 
 describe('TopNavBarComponent', () => {
   let component: TopNavBarComponent;
@@ -8,7 +11,15 @@ describe('TopNavBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TopNavBarComponent ]
+      declarations: [ TopNavBarComponent ],
+      imports: [
+        RouterTestingModule.withRoutes(
+          [
+            { path: 'hotels', component: DummyComponent },
+            { path: 'car-rental', component: DummyComponent }
+          ]
+        )
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +33,39 @@ describe('TopNavBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should be 3 tabs in nav bar', () => {
+    const tabs = fixture.debugElement.queryAll(By.css('.nav-link'))
+    expect(tabs.length).toEqual(3)
+  })
+
+  it( 'should navigate to / before any tab is clicked', () => {
+    const location = TestBed.get(Location);
+    expect(location.path()).toBe('');
+  })
+
+  it('should navigate to /hotels when HOTELS tab is clicked', () => {
+    const location = TestBed.get(Location);
+    const tabs = fixture.debugElement.queryAll(By.css('.nav-link'));
+    const hotelTab: HTMLLinkElement = tabs[1].nativeElement;
+    hotelTab.click()
+    fixture.detectChanges()
+    fixture.whenStable().then( () => {
+      expect(location.path()).toBe('/hotels')
+    })
+  })
+
+  it('should navigate to /car-rental when CAR RENTAL tab is clicked', () => {
+    const location = TestBed.get(Location);
+    const tabs = fixture.debugElement.queryAll(By.css('.nav-link'));
+    const carRentalTab: HTMLLinkElement = tabs[2].nativeElement;
+    carRentalTab.click()
+    fixture.detectChanges()
+    fixture.whenStable().then( () => {
+      expect(location.path()).toBe('/car-rental')
+    })
+  })
 });
+
+
+class DummyComponent {}
